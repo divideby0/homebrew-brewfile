b201d357f5b8b2b9019a8e5d5c5cdd59b8019a60
Cedric Hurst <cedric@spantree.net>
2023-01-21 11:19:44 -0600

    add additional asdf plugins

--- asdf-bootstrap-plugins.sh
+++ asdf-bootstrap-plugins.sh
@@ -6,7 +6,6 @@ asdf plugin add argo || true
 asdf plugin add goss || true
 asdf plugin add helm || true
 asdf plugin add k9s || true
-asdf plugin-add kfilt https://github.com/feniix/asdf-kfilt.git || true
 asdf plugin add kubectl || true
 asdf plugin add kustomize || true
 asdf plugin add nodejs || true
@@ -15,5 +14,15 @@ asdf plugin add skaffold || true
 asdf plugin add sops || true
 asdf plugin add stern || true
 asdf plugin add terraform || true
+asdf plugin-add cloudsql-proxy || true
+asdf plugin-add eksctl || true
+asdf plugin-add java || true
+asdf plugin-add kfilt https://github.com/feniix/asdf-kfilt.git || true
+asdf plugin-add kops || true
+asdf plugin-add kops || true
+asdf plugin-add kubectx || true
+asdf plugin-add pnpm || true
+asdf plugin-add python || true
+asdf plugin-add yarn || true
   
 # asdf plugin add kubefwd | true
